2927ba014937ee94708ee291cf18348894dff61b
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2023-10-18 12:00:20 +0530

    Logger Design Pattern

---
+++ LowLevelDesign/src/LLDChainResponsiblityDesignPattern/Main.java
@@ -0,0 +1,13 @@
+package LLDChainResponsiblityDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        LogProcessor logObject = new InfoLogProcessor(
+                new DebugLogProcessor
+                        (new ErrorLogProcessor(null)));
+
+        logObject.log(LogProcessor.INFO, "JUST A INFO");
+        logObject.log(LogProcessor.DEBUG, "DEBUGGING NEEDS HERE");
+        logObject.log(LogProcessor.ERROR, "ERROR OCCURED");
+    }
+}
