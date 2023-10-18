0a493364311d28c0ccd2df3ad2d67764cbc1f53b
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2023-10-18 12:05:15 +0530

    Logger Design Code

---
+++ LLDChainofResponsiblity/src/ErrorLogProcessor.java
@@ -0,0 +1,14 @@
+public class ErrorLogProcessor extends  LogProcessor{
+    ErrorLogProcessor(LogProcessor logProcessor)
+    {
+        super(logProcessor);
+    }
+
+    @Override
+    public void log(int logLevel, String message) {
+        if(logLevel == ERROR)
+            System.err.println("ERROR: "+message);
+       else
+        super.log(logLevel, message);
+    }
+}
